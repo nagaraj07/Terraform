feb41562be0eb84fdc01427f902c19c85f56c097
murali <immurali5>
2018-10-25 23:48:00 +0530

    added security group

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -9,4 +9,24 @@ resource "aws_instance" "murali" {
     key_name        = "murali"
     security_groups = ["nsg"]
 }
+resource "aws_security_group" "allow_all" {
+  name        = "allow_all"
+  description = "Allow all inbound traffic"
+  vpc_id      = "vpc-064n9448"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+    
+  }
+}
 
