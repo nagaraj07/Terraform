18c0aec0de0b5b32a049d52cdc3b2c1185fd1a8f
murali <immurali5>
2018-10-27 00:49:54 +0530

    java installation in tf

---
+++ java/var.tf
@@ -0,0 +1,31 @@
+variable "accesskey" {
+}
+
+variable "secretkey" {
+  
+}
+variable "location" {
+    default = "ap-south-1"
+  
+}
+
+variable "firewall" {
+    default = ["sec"]
+  
+}
+variable "ami" {
+    default = "ami-04ea996e7a3e7ad6b"
+  
+}
+variable "vpc" {
+    default = "vpc-fd683695"
+  
+}
+variable "private_key_path" {
+  
+}
+
+
+
+
+
