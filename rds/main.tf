175ee8a2d66bf2de56e5c18733df3a866e55645a
murali <immurali5>
2018-10-31 21:09:55 +0530

    codebuild to artifactory s3

--- rds/main.tf
+++ rds/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
   secret_key = "${var.secretkey}"
   region     = "ap-south-1"
 }
-resource "aws_db_instance" "default" {
+resource "aws_db_instance" "RDS" {
   allocated_storage    = 10
   storage_type         = "gp2"
   engine               = "mysql"
@@ -14,4 +14,8 @@ resource "aws_db_instance" "default" {
   username             = "foo"
   password             = "foobarbaz"
   parameter_group_name = "default.mysql5.7"
+}
+
+tags "RDS" {
+  
 }
