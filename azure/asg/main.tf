7c25a0f359fcdd78b748ebf0c179641b6d37078f
murali <immurali5>
2018-11-05 23:38:49 +0530

    azure security group

---
+++ azure/asg/main.tf
@@ -0,0 +1,33 @@
+provider "azurerm" {
+   subscription_id = "${var.subscriptionid}"
+   client_id       = "${var.clientid}"
+   client_secret   = "${var.clientsecret}"
+   tenant_id       = "${var.tenantid}"
+}
+
+resource "azurerm_resource_group" "test" {
+  name     = "acceptanceTestResourceGroup1"
+  location = "West US"
+}
+
+resource "azurerm_network_security_group" "murali" {
+  name                = "acceptanceTestSecurityGroup1"
+  location            = "${azurerm_resource_group.satya.location}"
+  resource_group_name = "${azurerm_resource_group.satya.name}"
+
+  security_rule {
+    name                       = "firewall"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags {
+    environment = "janasena"
+  }
+}
